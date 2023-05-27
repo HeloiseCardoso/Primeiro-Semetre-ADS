76339623da2929e877dfc19a477e37479e6a6232
Heloíse Cardoso <104039569+HeloiseCardoso@users.noreply.github.com>
2023-05-27 12:07:10 -0300

    Update NomeSobrenome.java

--- NomeSobrenome.java
+++ NomeSobrenome.java
@@ -17,6 +17,70 @@ public class Principal {
         String nomEsobrenome = nomeajustado + " " + sobrenomejustado;
         System.out.println("Nome e sobrenome: " + nomEsobrenome);
 
+    }
+}
+
+import java.util.Random;
+
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+
+
+
+
+    public String lerNome(){
+    Random random = new Random();
+        String auxiliar = " ";
+        for(int i = 0; i< 10; i++) {
+            int nome1 = random.nextInt(26) + 65;
+            char nomeChar = (char) nome1;
+            auxiliar = auxiliar + nomeChar;
+            nome = auxiliar;
+        }
+        return nome;
+    }
+
+
+    public String lerSobrenome(){
+        Random random = new Random();
+        String auxiliar = " ";
+        for(int i = 0; i< 15; i++) {
+            int nome = random.nextInt(26) + 65;
+            char nomeChar = (char) nome;
+            String aux = Character.toString(nomeChar);
+            auxiliar = auxiliar + aux;
+            sobrenome = auxiliar;
+        }
+        return sobrenome;
+    }
+
+
+
+    public String ajustaNome(String nome){
+        String retorno = nome.substring(1,2).toUpperCase() + nome.substring(2).toLowerCase();
+        return retorno;
+
+    }
+
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
 
 
     }
