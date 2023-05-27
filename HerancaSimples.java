b8011e7f748dd9f8469c17cd3c565ecda1c57b26
Heloíse Cardoso <104039569+HeloiseCardoso@users.noreply.github.com>
2023-05-27 12:10:55 -0300

    Add files via upload

---
+++ HerancaSimples.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+
+    Pessoa pessoa = new Pessoa();
+        System.out.println(pessoa.primeiroLetraNomeMaiuscula());
+        System.out.println(pessoa.ajustaNomes("heloise cardoso"));
+    }
+}
