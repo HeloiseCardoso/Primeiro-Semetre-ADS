a25192c3d409e42869e80819c08b3a6f4500c226
Heloíse Cardoso <104039569+HeloiseCardoso@users.noreply.github.com>
2023-05-27 12:02:07 -0300

    Add files via upload

---
+++ Lucro10%.java
@@ -0,0 +1,24 @@
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+
+
+        int codigo;
+        double precoMercadoria;
+        double precoVendaMercadoria;
+        int numeroVendas;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a quantidade de mercadorias");
+        int quantidade = scanner.nextInt();
+
+        System.out.println("Quantidade de Mercadorias que geraram lucro menor que 10%.");
+        int lucroMenor = scanner.nextInt();
+
+        //System.out.println("Determine a quantidade de Mercadorias que geraram lucro maior ou igual a 10% e menor ou igual a 20%."");
+
+
+    }
+}
