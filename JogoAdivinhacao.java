78df97eb486731e778d6edc954a6ffc872898e13
Heloíse Cardoso <104039569+HeloiseCardoso@users.noreply.github.com>
2023-05-27 12:09:36 -0300

    Update JogoAdivinhacao.java

--- JogoAdivinhacao.java
+++ JogoAdivinhacao.java
@@ -62,3 +62,128 @@ public class Jogo {
 
     }
 }
+
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Jogador {
+    private String nomeJogador;
+    private int[] palpites;
+
+    private boolean isVencedor = false;
+
+    static Random random = new Random();
+    //Um jogo deve possuir um ArrayList de jogadores, cujas informações serão armazenadas durante a execução do jogo.
+
+
+
+    public void gerarPalpites(int quantidadePalpites, int valorMinimo, int valorMaximo) {
+        this.palpites = new int[quantidadePalpites];
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < quantidadePalpites; i++) {
+
+            int palpite = Integer.parseInt(JOptionPane.showInputDialog("Digite seu papite:"));
+            JOptionPane.showMessageDialog(null, "Palpite: " + palpite);
+            //System.out.println("digite seu palpite");
+             //int palpite = scanner.nextInt();
+             this.palpites[i] = palpite;
+        }
+    }
+
+    public String getNomeJogador() {
+        return nomeJogador;
+    }
+
+    public void setNomeJogador(String nomeJogador) {
+        this.nomeJogador = nomeJogador;
+    }
+
+    public int[] getPalpites() {
+        return palpites;
+    }
+
+    public boolean isVencedor() {
+        return isVencedor;
+    }
+
+    public void setIsVencedor(boolean vencedor) {
+        isVencedor = vencedor;
+    }
+}
+
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Controle extends Jogador {
+
+        ArrayList<Jogador> jogadores;
+        public void iniciarJogo(int valorMinimo, int valorMaximo, int quantidadeJogadores, int quantidadePalpite) {
+
+            int numeroGerado = gerarNumero(valorMinimo, valorMaximo);
+            this.jogadores = gerarJogadores(valorMinimo, valorMaximo, quantidadeJogadores, quantidadePalpite);
+
+            for(int i = 0; i < this.jogadores.size(); i++) {
+                Jogador jogador = this.jogadores.get(i);
+
+                for(int j = 0 ; j < quantidadePalpite; j ++) {
+                    if(jogador.getPalpites()[j] == numeroGerado) {
+                        jogador.setIsVencedor(true);
+                        break;
+                    }
+                }
+
+            }
+
+        }
+
+        public void imprimirVencedores() {
+            System.out.println("Jogadores Vencedores:");
+            for(int i = 0; i < jogadores.size() ; i++) {
+                Jogador jogador = jogadores.get(i);
+                if(jogador.isVencedor()) {
+                    System.out.println(jogador.getNomeJogador());
+                }
+            }
+        }
+
+        private int gerarNumero(int valorMinimo, int valorMaximo) { // o metodo vai sortear os numero e coloca-los em um vetor
+            Random random = new Random();
+            int numeroGerado = random.nextInt((valorMaximo - valorMinimo)+1) + valorMinimo; //formula: aleatorio.nextInt((max - min) + 1) + min;
+            System.out.println("numeroGerado: " + numeroGerado);
+            return numeroGerado;
+        }
+
+        private ArrayList gerarJogadores(int valorMinimo, int valorMaximo, int quantidadeJogadores, int quantidadePalpite) {
+            Scanner scanner = new Scanner(System.in);
+            ArrayList<Jogador> jogadores = new ArrayList<>();
+
+            for (int i = 0; i < quantidadeJogadores; i++) {
+                Jogador jogador = new Jogador();
+                //System.out.println("Escreve o nome do jogador: ");
+               // jogador.setNomeJogador(scanner.next());
+
+                String NomeJogador = JOptionPane.showInputDialog("Nome do Jogador");
+                JOptionPane.showMessageDialog(null, "Nome: " + NomeJogador);
+                jogador.gerarPalpites(quantidadePalpite, valorMinimo, valorMaximo); //palpite e randomico
+                imprimirPalpite(jogador.getPalpites());
+                jogadores.add(jogador);
+            }
+            return jogadores;
+        }
+
+        private void imprimirPalpite(int[] palpites) {
+            for (int i = 0; i < palpites.length; i++) {
+                System.out.println(palpites[i]);
+            }
+        }
+
+}
+
+
+
