05088c7061cbe4a40c70f30cb45f37d85012a1f6
Heloíse Cardoso <104039569+HeloiseCardoso@users.noreply.github.com>
2023-05-27 11:56:46 -0300

    Exercício1 com vetor

---
+++ VetorEx3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VetorEx3 {
+    public static void main(String[] args) {
+
+                Scanner scanner = new Scanner(System.in);
+                int n = 0;
+                int vetor[];
+
+                while (n > 50000 ) {
+                    System.out.println("Numero de elementos da segunda linha: ");
+                    n = scanner.nextInt();
+                }
+
+                for( int i=0; i<n; i++){
+                    vetor[i]
+                }
+
+
+
+    }
+}
